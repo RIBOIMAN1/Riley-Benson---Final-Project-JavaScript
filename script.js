55e77be4436a71a58cce14c2513dd582bdf01d74
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-12-02 16:06:25 -0800

    script.js: - Added a "let" command that sets the starting player of the game to "X." - Added a "let" command that represents the 3x3 grid on the board, giving 9 quotation mark sets. - Added a "let" command that tracks the activity of the game. - Added two "let" command that sets the score of players "X" and "O" to 0 respectively.
    main.css:
    - Added a 75% cyan, 25% blue hex code background color to the button.
    - Added white text color for the button.
    - Removed the border from the button.
    - Added a 10px 20px padding for the button that makes it look more rectangular and clean.
    - Added a command that multiplies the size of the font by 1.
    - Added a command that changes the cursor type to pointer whenever you hover over the button with the cursor.
    - Added a 5px border radius to the button to round the corners.
    - Added an ease background transition effect of 0.3s to the background color so that there is a smooth color transition when interacting with the button.
    - Converted all text to uppercase.
    - Bolded all of the text in the button.
    - Removed the outline of the button whenever it is focused.
    - Added a 20px bottom margin to give slight separation between the button and the scoreboard.
    - Added a background color that is slightly darker blue when hovering over the button.
    - Added an RGBA colored black shadow with dimensions being 0 4px 6px, giving a slight shadow effect when hovering over the button.
    - Made the button shrink to 95% size while clicking on the button.
    - Made the background an even darker shade of blue whenever the button is being clicked on.

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ document.addEventListener('DOMContentLoaded', () => { // Selects important eleme
     const playAgainButton = document.getElementById('button-play-again'); // Code for the "Play again!" button
     const scoreboardX = document.getElementById('scoreboard-x'); // Scoreboard for player "X"
     const scoreboardO = document.getElementById('scoreboard-o'); // Scoreboard for player "O"
+    // Initializes the game state variables
+    let currentPlayer = 'X'; // Starts the game with player "X"
+    let gameBoard = ['', '', '', '', '', '', '', '', '']; // Represents the 3x3 Tic-Tac-Toe board
+    let gameActive = true; // Tracks whether or not the game is active
+    let scoreX = 0; // Score for player "X" to start at 0
+    let scoreO = 0; // Score for player "O" to start at 0
 });
